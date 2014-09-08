7d09f359eaf6e6e6b828457cd8995894b0603f27
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-08 21:16:32 +0200

    Line breaks

--- src/lib/eventmap.js
+++ src/lib/eventmap.js
@@ -1,9 +1,9 @@
-define('eventmap', function() {
-  var EventMap = function() {
+define('eventmap', function () {
+  var EventMap = function () {
     this.events = {};
   };
 
-  ['before', 'after', {name: 'now', method: 'on'}].forEach(function(val) {
+  ['before', 'after', {name: 'now', method: 'on'}].forEach(function (val) {
     if (typeof val === 'string') {
       val = {
         name: val,
@@ -11,22 +11,22 @@ define('eventmap', function() {
       };
     }
 
-    EventMap.prototype[val.method] = function(name, callback) {
+    EventMap.prototype[val.method] = function (name, callback) {
       this.events[name] = this.events[name] || {};
       this.events[name][val.name] = this.events[name][val.name] || [];
       this.events[name][val.name].push(callback);
     };
   });
 
-  EventMap.prototype.off = function(name) {
+  EventMap.prototype.off = function (name) {
     delete this.events[name];
   };
 
-  EventMap.prototype.trigger = function() {
+  EventMap.prototype.trigger = function () {
     var name = arguments[0];
     var args = [].slice.call(arguments, 1);
 
-    ['before', 'now', 'after'].forEach(function(val) {
+    ['before', 'now', 'after'].forEach(function (val) {
       if (this.events[name] && this.events[name][val]) {
         this.events[name][val].apply({}, args);
       }
